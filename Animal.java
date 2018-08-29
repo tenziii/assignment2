1fc3a080ba3bce59e4fe036a26cf77e8461651aa
tenziii <42807735+tenziii@users.noreply.github.com>
2018-08-29 22:22:50 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,22 @@
+abstract class Animal
+{
+void spaeak()
+{
+System.out.println(" ");
+}
+}
+class Dog extends Animal
+{
+void speak()
+{
+System.out.println(" ");
+}
+}
+class Cat extends Animal
+{
+void speak()
+{
+System.out.println(" ");
+}
+}
+}
---
+++ Area.java
@@ -0,0 +1,15 @@
+class Area{
+  int lenght,breath;
+  Main(int l,int b)
+  {
+    lenght=l;
+    breath=b;
+    int a=lenght*breath;
+    System.out.println("The area of rectangle is:"+a);
+  }
+}
+class Area {
+  public static void main(String[] args) {
+   Main ob=new Main(15,25);
+  }
+}
